4bb7dc1df69e8eff784df11162b5ef3f712cce9d
Joseph DeWeese <inclinedtactics@icloud.com>
2024-04-25 14:30:16 -0500

    Initial Commit

---
+++ WeldCalc/WeldCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeldCalcApp.swift
+//  WeldCalc
+//
+//  Created by Joseph DeWeese on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct WeldCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
